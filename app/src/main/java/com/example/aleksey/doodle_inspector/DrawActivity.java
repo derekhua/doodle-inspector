f1763e0006716a258017e23d26578466ab674bed
xin-he <xinhe@ufl.edu>
2016-01-24 00:56:42 -0500

    added android socket.io code to connect to EC2

--- app/src/main/java/com/example/aleksey/doodle_inspector/DrawActivity.java
+++ app/src/main/java/com/example/aleksey/doodle_inspector/DrawActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+import java.net.*;
+import com.github.nkzawa.emitter.Emitter;
+import android.util.Log;
 
 public class DrawActivity extends ActionBarActivity {
+
+    private Socket mSocket;
 
     private DrawView drawView;
     private ImageButton currPaint;
@@ -15,7 +21,35 @@ public class DrawActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw);
-        drawView = (DrawView)findViewById(R.id.drawing);
+        try {
+
+            mSocket = IO.socket("http://ec2-54-165-233-14.compute-1.amazonaws.com:3000");
+            mSocket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    Log.d("DrawActivity: ", "SOCKETLOG: socket connected");
+                    mSocket.emit("message", "testing");
+                }
+                // this is the emit from the server
+            }).on("hello", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    for (int i = 0; i < args.length; i++) {
+                        Log.d("DrawActivity: ", "SOCKETLOG: " + args[i]);
+                    }
+                }
+            }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    Log.d("DrawActivity: ", "SOCKETLOG: socket disconnected");
+                }
+            });
+            mSocket.connect();  // initiate connection to socket server
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        drawView = (DrawView) findViewById(R.id.drawing);
         LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
         currPaint = (ImageButton)paintLayout.getChildAt(0);
         currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
@@ -34,5 +68,13 @@ public class DrawActivity extends ActionBarActivity {
             currPaint=(ImageButton)view;
         }
 
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mSocket.disconnect();
+        Log.d("DrawActivity: ", "SOCKETLOG: Socket off");
     }
 }
