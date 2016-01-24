27bb6b894b722d7dc0f26d616ade048a2ff6ca8f
xin-he <xinhe@ufl.edu>
2016-01-24 01:18:37 -0500

    removed emitters when DrawActivity is destroyed

--- app/src/main/java/com/example/aleksey/doodle_inspector/DrawActivity.java
+++ app/src/main/java/com/example/aleksey/doodle_inspector/DrawActivity.java
@@ -14,6 +14,17 @@ import android.util.Log;
 public class DrawActivity extends ActionBarActivity {
 
     private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket("http://ec2-54-165-233-14.compute-1.amazonaws.com:3000");
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    Emitter.Listener connectEmitter;
+    Emitter.Listener helloEmitter;
+    Emitter.Listener disconnectEmitter;
 
     private DrawView drawView;
     private ImageButton currPaint;
@@ -21,33 +32,31 @@ public class DrawActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw);
-        try {
+        connectEmitter = new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Log.d("DrawActivity: ", "SOCKETLOG: socket connected");
+                mSocket.emit("message", "testing");
+            }
+        };
+        helloEmitter = new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                for (int i = 0; i < args.length; i++) {
+                    Log.d("DrawActivity: ", "SOCKETLOG: " + args[i]);
+                }
+            }
+        };
+        disconnectEmitter = new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Log.d("DrawActivity: ", "SOCKETLOG: socket disconnected");
+            }
+        };
+        mSocket.on(Socket.EVENT_CONNECT, connectEmitter)
+        .on("hello", helloEmitter).on(Socket.EVENT_DISCONNECT, disconnectEmitter);
 
-            mSocket = IO.socket("http://ec2-54-165-233-14.compute-1.amazonaws.com:3000");
-            mSocket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
-                @Override
-                public void call(Object... args) {
-                    Log.d("DrawActivity: ", "SOCKETLOG: socket connected");
-                    mSocket.emit("message", "testing");
-                }
-                // this is the emit from the server
-            }).on("hello", new Emitter.Listener() {
-                @Override
-                public void call(Object... args) {
-                    for (int i = 0; i < args.length; i++) {
-                        Log.d("DrawActivity: ", "SOCKETLOG: " + args[i]);
-                    }
-                }
-            }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
-                @Override
-                public void call(Object... args) {
-                    Log.d("DrawActivity: ", "SOCKETLOG: socket disconnected");
-                }
-            });
-            mSocket.connect();  // initiate connection to socket server
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
+        mSocket.connect();  // initiate connection to socket server
 
         drawView = (DrawView) findViewById(R.id.drawing);
         LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
@@ -73,8 +82,10 @@ public class DrawActivity extends ActionBarActivity {
     @Override
     public void onDestroy() {
         super.onDestroy();
-
         mSocket.disconnect();
+        mSocket.off(Socket.EVENT_CONNECT, connectEmitter);
+        mSocket.off(Socket.EVENT_DISCONNECT, disconnectEmitter);
+        mSocket.off("hello", helloEmitter);
         Log.d("DrawActivity: ", "SOCKETLOG: Socket off");
     }
 }
